a3a40f1840dd5c04cdc499c0389f14a00f9e6c75
Dean Calhoun <dcalhoun@crhtc43.hpc.ucar.edu>
2024-11-21 08:23:01 -0700

    update preprocessing script

--- preprocessing.py
+++ preprocessing.py
@@ -10,7 +10,7 @@ from itertools import product
 def create_ifs_forecast_folders(
     target_directory,
     start = '2016-01-01',
-    end = '2023-12-31',
+    end = '2024-01-01',
     params = ['t2m'],
     init_times = ["00", "12"],
     lead_times = ["0", "6", "12", "24", "48", "72", "96", "120", "168", "240"]
@@ -24,47 +24,48 @@ def create_ifs_forecast_folders(
         for init_time in init_times:
             for lead_time in lead_times:
                 for date in dates:
-                    valid_time = date + relativedelta(hours=int(init_time)) + relativedelta(hours=int(lead_time))
-                    year = valid_time.strftime("%Y")
-                    month = 
+                    year = date.strftime("%Y")
+                    month = date.strftime("%m")
                     path = '/'.join([target_directory, param, init_time, lead_time, year, month])
                     os.makedirs(path, exist_ok=True)
 
 def create_ifs_analysis_folders(
     target_directory,
     start = '2016-01-01',
-    end = '2023-12-31',
-    params = ['t2m'],
-    init_times = ["00", "06", "12", "18"]
+    end = '2024-01-01',
+    params = ['t2m']
 ):
     '''
     Creates directories needed to store ECMWF IFS analysis data
     '''
-    dates = pd.date_range(start = start, end = end, freq = 'MS')
+    dates = pd.date_range(start = start, end = end, freq = 'D')
     for param in params:
-        for year, month in product(np.unique(dates.strftime("%Y")), np.unique(dates.strftime("%m"))):
-            for init_time in init_times:
-                path = '/'.join([target_directory, param, init_time, year, month])
-                os.makedirs(path, exist_ok=True)
+        for date in dates:
+            year = date.strftime("%Y")
+            month = date.strftime("%m")
+            day = date.strftime("%d")
+            path = '/'.join([target_directory, param, year, month, day])
+            os.makedirs(path, exist_ok=True)
 
-def create_era_climatology_folders(target_directory, params = ['t2m'], times = ["00", "06", "12", "18"]):
+def create_era_climatology_folders(target_directory, params = ['t2m']):
     '''
     Creates directories needed to store ECMWF ERA5 daily climatology data
     '''
     dates = pd.date_range(start = "2000-01-01", end = "2000-12-31", freq = 'MS')
     for param in params:
-        for month, day in product(np.unique(dates.strftime("%m")), np.unique(dates.strftime("%d"))):
-            for time in times:
-                path = '/'.join([target_directory, param, month, day, time])
-                os.makedirs(path, exist_ok=True)
+        for date in dates:
+            month = date.strftime("%m")
+            day = date.strftime("%d")
+            path = '/'.join([target_directory, param, month, day])
+            os.makedirs(path, exist_ok=True)
                         
     return
 
 def retrieve_ifs_forecast(
     target_directory,
-    params = {"t2m": "167.128"},
     start = '2016-01-01',
-    end = '2023-12-31',
+    end = '2024-01-01',
+    params = {"t2m": "167.128"},
     init_times = ["00", "12"],
     lead_times = ["0", "6", "12", "24", "48", "72", "96", "120", "168", "240"],
     bounds = ["45","-85","35","-70"]
@@ -76,8 +77,8 @@ def retrieve_ifs_forecast(
     create_ifs_forecast_folders(
         target_directory = target_directory,
         start = start,
-        params = params.keys(),
         end = end,
+        params = params.keys(),
         init_times = init_times,
         lead_times = lead_times
     )
@@ -95,27 +96,33 @@ def retrieve_ifs_forecast(
                     year = valid_time.strftime("%Y")
                     month = valid_time.strftime("%m")
                     day = valid_time.strftime("%d")
+                    hour = valid_time.strftime("%H")
                     date_str = valid_time.strftime("%Y-%m-%d")
-                    path = '/'.join([target_directory, param, init_time, lead_time, year, month, f'ifs_fc_{param}_{year}_{month}_{day}.grib'])
-                    server.execute({
-                        'class': "od",
-                        'type': "fc",
-                        'stream': "oper",
-                        'expver': "1",
-                        'repres': "gg",
-                        'levtype': "sfc",
-                        'param': params[param],
-                        'time': init_time,
-                        'step': lead_time,
-                        'domain': "g",
-                        'resol': "auto",
-                        'area': '/'.join(bounds),
-                        'grid': "0.125/0.125",
-                        'padding': "0",
-                        'expect': "any",
-                        'date': date_str
-                    },
-                        path)
+                    path = '/'.join([target_directory, param, init_time, lead_time, year, month, f'ifs_fc_{param}_{year}_{month}_{day}_{hour}z.grib'])
+                    try:
+                        server.execute({
+                            'class': "od",
+                            'type': "fc",
+                            'stream': "oper",
+                            'expver': "1",
+                            'repres': "gg",
+                            'levtype': "sfc",
+                            'param': params[param],
+                            'time': init_time,
+                            'step': lead_time,
+                            'domain': "g",
+                            'resol': "auto",
+                            'area': '/'.join(bounds),
+                            'grid': "0.125/0.125",
+                            'padding': "0",
+                            'expect': "any",
+                            'date': date_str
+                        },
+                            path)
+                        
+                    except Exception:
+                        print(f'Unable to retrieve forecast data for {"_".join([init_time,lead_time,date.strftime("%Y-%m-%d")])}')
+                        pass
     
                     # Convert to netcdf
                     subprocess.run(f'module load eccodes && grib_to_netcdf -o {path[:-4]+"nc"} {path}', shell=True)
@@ -128,8 +135,8 @@ def retrieve_ifs_analysis(
     target_directory,
     params = {"t2m": "167.128"},
     start = '2016-01-01',
-    end = '2023-12-31',
-    init_times = ["00", "06", "12", "18"],
+    end = '2024-01-10',
+    times = ["00", "06", "12", "18"],
     bounds = ["45","-85","35","-70"]
 ):
     '''
@@ -140,8 +147,7 @@ def retrieve_ifs_analysis(
         target_directory = target_directory,
         params = params.keys(), 
         start = start,
-        end = end,
-        init_times = init_times
+        end = end
     )
 
     # Establish API connection to ECMWF MARS archive
@@ -150,32 +156,37 @@ def retrieve_ifs_analysis(
     # Retrieve data
     dates = pd.date_range(start = start, end = end, freq = 'D')
     for param in params.keys():
-        for init_time in init_times:
-            for date in dates:
+        for date in dates:
+            for time in times:
                 year = date.strftime("%Y")
                 month = date.strftime("%m")
                 day = date.strftime("%d")
                 date_str = date.strftime("%Y-%m-%d")
-                path = '/'.join([target_directory, param, init_time, year, month, f'ifs_an_{param}_{year}_{month}_{day}.grib'])
-                server.execute({
-                    'class': "od",
-                    'type': "an",
-                    'stream': "oper",
-                    'expver': "1",
-                    'repres': "gg",
-                    'levtype': "sfc",
-                    'param': params[param],
-                    'time': init_time,
-                    'step': "0",
-                    'domain': "g",
-                    'resol': "auto",
-                    'area': '/'.join(bounds),
-                    'grid': "0.125/0.125",
-                    'padding': "0",
-                    'expect': "any",
-                    'date': date_str
-                },
-                    path)
+                path = '/'.join([target_directory, param, init_time, year, month, day, f'ifs_an_{param}_{year}_{month}_{day}_{time}z.grib'])
+                try:
+                    server.execute({
+                        'class': "od",
+                        'type': "an",
+                        'stream': "oper",
+                        'expver': "1",
+                        'repres': "gg",
+                        'levtype': "sfc",
+                        'param': params[param],
+                        'time': time,
+                        'step': "0",
+                        'domain': "g",
+                        'resol': "auto",
+                        'area': '/'.join(bounds),
+                        'grid': "0.125/0.125",
+                        'padding': "0",
+                        'expect': "any",
+                        'date': date_str
+                    },
+                        path)
+                    
+                except Exception:
+                        print(f'Unable to retrieve analysis data for {date.strftime("%Y-%m-%d-")+time+"z"}')
+                        pass
     
                 # Convert to netcdf
                 subprocess.run(f'module load eccodes && grib_to_netcdf -o {path[:-4]+"nc"} {path}', shell=True)
@@ -195,9 +206,11 @@ def retrieve_era5_climatology(
     '''
     # Ensure directories exist
     create_era_climatology_folders(
-        target_directory=target_directory, params=params.keys(),
-        start=start,
-        end=end
+        target_directory = target_directory,
+        params = params.keys(),
+        start = start,
+        end = end,
+        times = ["00", "06", "12", "18"]
     )
 
     # Establish API connection to ECMWF MARS archive
@@ -207,44 +220,115 @@ def retrieve_era5_climatology(
     dates = pd.date_range(start = "2000-01-01", end = "2000-12-31", freq = 'MS')
     for param in params.keys():
         for date in dates:
-            month = date.strftime("%m")
-            day = date.strftime("%d")
-            date_str = date.strftime("2000-%m-%d")
-            path = '/'.join([target_directory, param, year, month, f'era5_dacl_{param}_{month}_{day}.grib'])
-            server.execute({
-                'class': "ea",
-                'type': "em",
-                'stream': "dacl",
-                'expver': "1",
-                'levtype': "sfc",
-                'param': "167.128",
-                'time': "00/06/12/18",
-                'area': '/'.join(bounds),
-                'grid': "0.25/0.25",
-                'padding': "0",
-                'expect': "any",
-                'date': date_str
-            },
-                path)
+            for time in times:
+                month = date.strftime("%m")
+                day = date.strftime("%d")
+                date_str = date.strftime("2000-%m-%d")
+                path = '/'.join([target_directory, param, month, day, f'era5_dacl_{param}_{month}_{day}_{time}z.grib'])
+                try:
+                    server.execute({
+                        'class': "ea",
+                        'type': "em",
+                        'stream': "dacl",
+                        'expver': "1",
+                        'levtype': "sfc",
+                        'param': "167.128",
+                        'time': time,
+                        'area': '/'.join(bounds),
+                        'grid': "0.25/0.25",
+                        'padding': "0",
+                        'expect': "any",
+                        'date': date_str
+                    },
+                        path)
 
-            # Convert to netcdf
-            subprocess.run(f'module load eccodes && grib_to_netcdf -o {path[:-4]+"nc"} {path}', shell=True)
-        
-            # Delete grib file
-            subprocess.run(f'rm {path}')
+                except Exception:
+                        print(f'Unable to retrieve ERA data for {date.strftime("%m-%d-")+time+"z"}')
+                        pass
+    
+                # Convert to netcdf
+                subprocess.run(f'module load eccodes && grib_to_netcdf -o {path[:-4]+"nc"} {path}', shell=True)
+            
+                # Delete grib file
+                subprocess.run(f'rm {path}')
     
     return
 
 def calculate_forecast_anomalies():
+    '''
+    Calculates the difference between forecast and climatology
+    '''
+
+    # Create anomaly directories
+
+    # Iterate over dates
+
+    # Read in forecast and climatology
+
+    # Ensure grid alignment
+
+    # Subtract climatology
+
+    # Save out
+    
     return
 
 def calculate_analysis_anomalies():
+    '''
+    Calculates the differencd between analysis and climatology
+    '''
+
+    # Create anomaly directories
+
+    # Iterate over dates
+
+    # Read in analysis and climatology
+
+    # Ensure grid alignment
+
+    # Subtract climatology
+
+    # Save out
+    
     return
 
 def calculate_RMSE():
+    '''
+    Calculates the RMSE between forecast anomalies and analysis anomalies
+    '''
+
+    # Create directories
+
+    # Iterate over dates
+
+    # Read in forecast and analysis anomalies
+
+    # Ensure grid alignment
+
+    # Calculate RMSE
+
+    # Save out
+    
     return
 
 def calculate_ACC():
+    '''
+    Calculates the anomaly correlation coefficient,
+    the correlation between forecast anomalies and analysis anomalies
+    '''
+
+    # Create directories
+
+    # Iterate over dates
+
+    # Read in forecast and analysis anomalies
+
+    # Ensure grid alignment
+
+    # Calculate ACC
+
+    # Save out
+    
     return
 
 if __name__ == '__main__':
