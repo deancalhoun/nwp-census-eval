109ef3886bef7ae11b2a06e7f7be1cdc8083b397
Dean Calhoun <deanslcalhoun@gmail.com>
2025-03-04 10:41:01 -0700

    remove unnecessary scripts

--- src/get_aifs.py
+++
@@ -1,4 +0,0 @@
-import preprocessing
-
-dir = '/glade/derecho/scratch/dcalhoun/ecmwf_aifs'
-preprocessing.retrieve_aifs_forecast(dir)
--- src/get_era.py
+++
@@ -1,4 +0,0 @@
-import preprocessing
-
-dir = '/glade/derecho/scratch/dcalhoun/ecmwf_era5'
-preprocessing.retrieve_era5_data(dir)
