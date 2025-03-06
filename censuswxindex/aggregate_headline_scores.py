c4226f813e50ad0a9415a1b8a595e9fec1e448e4
Dean Calhoun <deanslcalhoun@gmail.com>
2025-03-06 14:53:34 -0700

    rename and update aggregation script

--- censuswxindex/aggregate_headline_scores.py
+++ censuswxindex/aggregate_headline_scores.py
@@ -1,20 +1,33 @@
-import censuswxindex as cwi
+import os
+import sys
 import xarray as xr
 import geopandas as gpd
 import pandas as pd
-import os
 import matplotlib.pyplot as plt
 
-data_path='/glade/derecho/scratch/dcalhoun/ecmwf/ifs/fc/t2m/00/06/2016/01/ifs_fc_t2m_2016_01_01_06z.nc'
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import censuswxindex as cwi
+
+## Parameters
+data_dir = ''
+model_name = ''
+param = ''
+path = os.path.join(data_dir, '*.nc')
+data_path_list = glob.glob(path)
 shapefile_path='/glade/derecho/scratch/dcalhoun/census/shapefiles/nhgis0001_shapefile_tl2023_us_county_2023/US_county_2023.shp'
 target_dir='/glade/derecho/scratch/dcalhoun/aggregated'
 outfile = os.path.join(target_dir, os.path.splitext(data_path)[1][:-3] + "_aggregated.shp")
 bounds = [50, -130, 25, -40]
 
-ds = xr.open_dataset(data_path).isel(time=0)
+## Read shapefile
 gdf = gpd.read_file(shapefile_path).to_crs('WGS84')
 gdf["INTPTLAT"] = pd.to_numeric(gdf["INTPTLAT"])
 gdf["INTPTLON"] = pd.to_numeric(gdf["INTPTLON"])
+
+## Read data
+for data_path in data_path_lis
+ds = xr.open_dataset(data_path).isel(time=0)
 
 if not os.path.exists(outfile):
     cwi.aggregate_to_geography(ds=ds, gdf=gdf, outfile=outfile, bounds=bounds)
