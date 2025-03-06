691fc58ff09d9f799cd81b5aba74acf6845cc36e
Dean Calhoun <67117902+deancalhoun@users.noreply.github.com>
2025-03-06 17:42:07 -0500

    Update aggregate_headline_scores.py

--- censuswxindex/aggregate_headline_scores.py
+++ censuswxindex/aggregate_headline_scores.py
@@ -13,8 +13,8 @@ data_dir = '/glade/derecho/scratch/dcalhoun/ecmwf/scores'
 scores = ['rmse', 'acc']
 model_names = ['ifs', 'aifs']
 lead_times = ['00', '06', '12', '24', '48', '72', '96', '120', '168', '240']
-shapefile_path='/glade/derecho/scratch/dcalhoun/census/shapefiles/nhgis0001_shapefile_tl2023_us_county_2023/US_county_2023.shp'
-target_dir='/glade/derecho/scratch/dcalhoun/aggregated'
+shapefile_path = '/glade/derecho/scratch/dcalhoun/census/shapefiles/nhgis0001_shapefile_tl2023_us_county_2023/US_county_2023.shp'
+save_dir = '/glade/derecho/scratch/dcalhoun/aggregated'
 bounds = [50, -130, 25, -40]
 
 ## Read shapefile
@@ -26,10 +26,11 @@ gdf["INTPTLON"] = pd.to_numeric(gdf["INTPTLON"])
 for score in scores:
     for model_name in model_names:
         for lead_time in lead_times:
-            data_path_lis = [os.path.join(data_dir, model_name, score, f'{model_name}_{score}_{lead_time}.nc')]
-            for data_path in data_path_lis
-                ds = xr.open_dataset(data_path).isel(time=0)
-                outfile = os.path.join(target_dir, os.path.splitext(data_path)[1][:-3] + "_aggregated.shp")
+            path = os.path.join(data_dir, score, model_name, lead_time, '*.nc')
+            data_path_list = glob.glob(path)
+            for data_path in data_path_list
+                ds = xr.open_dataset(data_path)
+                outfile = os.path.join(save_dir, os.path.split(data_path)[-1][:-3] + "_aggregated.shp")
                 if not os.path.exists(outfile):
                     cwi.aggregate_to_geography(ds=ds, gdf=gdf, outfile=outfile, bounds=bounds)
      
