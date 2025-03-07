d088f7ef5931c67d01745e616fcaa18291bc5323
Dean Calhoun <deanslcalhoun@gmail.com>
2025-03-07 07:36:18 -0700

    update logging in data retrieval functions

--- censuswxindex/get_ecmwf_data.py
+++ censuswxindex/get_ecmwf_data.py
@@ -19,7 +19,7 @@ params = {'t2m': '167.128'}
 init_times = ['00', '12']
 lead_times = ['00', '06', '12', '18', '24', '48', '72', '96', '120', '168', '240']
 valid_times = ['00', '06', '12', '18']
-grids = ['0.25', '0.125']
+grids = ['0.125']
 bounds = ['90','-180','0','0'] # Northwest Quadrant
 
 # Retrieve data
