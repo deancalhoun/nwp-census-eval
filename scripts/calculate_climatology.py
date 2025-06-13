503b9db6be53d3c10d2a5ec2faabb81fbd287de7
Dean Calhoun <deanslcalhoun@gmail.com>
2025-06-13 11:43:47 -0600

    update climatology script

--- scripts/calculate_climatology.py
+++ scripts/calculate_climatology.py
@@ -1,9 +1,7 @@
 import os
 import sys
-
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
-import censuswxindex as cwi
+from censuswxindex.preprocessing.climatology import calculate_era5_climatology
 
 ## Define parameters
 era_dir = '/glade/campaign/collections/rda/data/d633000/e5.oper.an.sfc/'
@@ -13,7 +11,7 @@ start_era = '1991-01-01'
 end_era = '2020-12-31'
 
 ## Calculate ERA5 climatology
-era_files = cwi.calculate_era5_climatology(
+calculate_era5_climatology(
     era_dir = era_dir,
     save_dir = era_save_dir,
     params = era_params,
