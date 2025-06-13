ed980654f131c8645155f899846ec05411824851
Dean Calhoun <deanslcalhoun@gmail.com>
2025-06-13 13:04:36 -0600

    update climatology script

--- scripts/calculate_climatology.py
+++ scripts/calculate_climatology.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from censuswxindex.preprocessing.climatology import calculate_era5_climatology
 
 ## Define parameters
