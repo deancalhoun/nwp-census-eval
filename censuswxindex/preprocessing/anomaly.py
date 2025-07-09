1a4033771b8966596a230ae7b989529ab7eea146
Dean Calhoun <deanslcalhoun@gmail.com>
2025-07-09 13:25:47 -0600

    add anomaly calculation script

---
+++ censuswxindex/preprocessing/anomaly.py
@@ -0,0 +1,17 @@
+import os
+import glob
+import xarray as xr
+import numpy as np
+import pandas as pd
+
+def calculate_ifs_t2m_fc_anomaly(fc_dir, clim_path, save_dir, year, month, init_time, lead_time):
+    '''
+    
+    '''
+    return
+
+def calculate_ifs_t2m_an_anomaly(an_dir, clim_path, save_dir, year, month, valid_time):
+    '''
+    
+    '''
+    return
